78e8ef2b4562db585f0bb61a766add55252b0661
wanderingmind <wanderingmind@kwandary.com>
2020-05-18 23:25:21 -0400

    replace mnist with dummy data

--- tensorflow_v1/examples/3_NeuralNetworks/minimal.py
+++ tensorflow_v1/examples/3_NeuralNetworks/minimal.py
@@ -1,15 +1,11 @@
 """ Minimal Tensorflow example
 """
 import tensorflow as tf
-
-# Import MNIST data
-from tensorflow.examples.tutorials.mnist import input_data
-
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=False)
-num_classes = 10 # MNIST total classes (0-9 digits)
+import numpy as np
+import random
 
 # Create the neural network
-def conv_net(x_dict, n_classes, is_training):
+def conv_net(x_dict):
     # Define a scope for reusing the variables
     with tf.variable_scope('ConvNet'):
         # TF Estimator input is a dict, in case of multiple inputs
@@ -29,28 +25,24 @@ def conv_net(x_dict, n_classes, is_training):
         fc1 = tf.contrib.layers.flatten(conv1)
 
         # Fully connected layer (in tf contrib folder for now)
-        fc1 = tf.layers.dense(fc1, 1024)
-
-        # Output layer, class prediction
-        out = tf.layers.dense(fc1, n_classes)
+        out = tf.layers.dense(fc1, 1024)
 
     return out
-
 
 # Define the model function (following TF Estimator Template)
 def model_fn(features, labels, mode):
     # Build the neural network
     # Because Dropout have different behavior at training and prediction time, we
     # need to create 2 distinct computation graphs that still share the same weights.
-    logits_train = conv_net(features, num_classes, is_training=True)
+    logits_train = conv_net(features)
+    
     pred_classes = tf.argmax(logits_train, axis=1)
 
     # Define loss and optimizer
     loss_op = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(
         logits=logits_train, labels=tf.cast(labels, dtype=tf.int32)))
     optimizer = tf.train.AdamOptimizer()
-    train_op = optimizer.minimize(loss_op,
-                                  global_step=tf.train.get_global_step())
+    train_op = optimizer.minimize(loss_op, global_step=tf.train.get_global_step())
 
     # Evaluate the accuracy of the model
     acc_op = tf.metrics.accuracy(labels=labels, predictions=pred_classes)
@@ -74,8 +66,14 @@ run_config = tf.estimator.RunConfig(session_config = session_config)
 model = tf.estimator.Estimator(model_fn, model_dir="/tmp/example", config=run_config)
 
 # Define the input function for training
+dummy_xdata = np.random.rand(10, 784)
+dummy_ydata = np.random.randint(10, size=(10)).astype('uint8')
+
+xval = {'images': dummy_xdata}
+yval = dummy_ydata
+
 input_fn = tf.estimator.inputs.numpy_input_fn(
-    x={'images': mnist.train.images}, y=mnist.train.labels,
+    x=xval, y=yval,
     batch_size=1, num_epochs=None, shuffle=False)
 # Train the Model
 model.train(input_fn, steps=1)
